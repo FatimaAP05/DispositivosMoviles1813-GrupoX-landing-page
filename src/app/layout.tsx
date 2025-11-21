9d5f333022441f56826e9c8e02db8fe5c8c2c51f
Fatima andrea asmad padilla <asmadpadillaf@gmail.com>
2025-11-21 00:11:58 +0000

    al hacer el deploy me sale esto https---github.com-DispositivosMoviles-1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { I18nProvider } from '@/context/i18n-context';
 export const metadata: Metadata = {
   title: 'Vitalia',
   description: 'Connecting Families, Enhancing Care.',
+  icons: {
+    icon: 'https://media.discordapp.net/attachments/1286768073453342767/1441098476266983495/logo.png?ex=69208f0b&is=691f3d8b&hm=d9f95a426a248bc51ee8fcbd6277621ffdd2c9cf747905e415d6a391f797e050&=&format=webp&quality=lossless&width=750&height=750',
+  },
 };
 
 export default function RootLayout({
